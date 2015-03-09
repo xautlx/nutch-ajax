7ac1080fc8f843830d1b2555d4547bdbdc639678
Li Xia <xautlx@hotmail.com>
2015-03-09 14:40:13 +0800

    在s2jh index插件中添加拼音和基于metadata形式传递获取自定义业务属性索引演示

--- apache-nutch-2.3/src/plugin/index-s2jh/src/java/org/apache/nutch/indexer/s2jh/AbstractIndexingFilter.java
+++ apache-nutch-2.3/src/plugin/index-s2jh/src/java/org/apache/nutch/indexer/s2jh/AbstractIndexingFilter.java
@@ -1,18 +1,23 @@
 package org.apache.nutch.indexer.s2jh;
 
+import java.nio.ByteBuffer;
 import java.util.Collection;
+import java.util.HashSet;
 
+import org.apache.avro.util.Utf8;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.nutch.indexer.IndexingException;
 import org.apache.nutch.indexer.IndexingFilter;
 import org.apache.nutch.indexer.NutchDocument;
+import org.apache.nutch.parse.s2jh.AbstractHtmlParseFilter;
 import org.apache.nutch.storage.WebPage;
+import org.apache.nutch.util.Bytes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * 
- * @author EMAIL:xautlx@hotmail.com , QQ:2414521719
+ * @author EMAIL:s2jh-dev@hotmail.com , QQ:2414521719
  *
  */
 public abstract class AbstractIndexingFilter implements IndexingFilter {
@@ -36,15 +41,42 @@ public abstract class AbstractIndexingFilter implements IndexingFilter {
         LOG.debug("Invoking  indexer {} for url: {}", this.getClass().getName(), url);
 
         if (doc == null) {
-            LOG.debug("Skipped as NutchDocument doc is null");
+            LOG.debug("Skipped index as NutchDocument doc is null");
             return doc;
+        }
+
+        if (!isMatchedForParseIndex(url)) {
+            LOG.debug("Skipped index as URL NOT match any htmlParseFilter.");
+            return null;
         }
 
         return filterInternal(doc, url, page);
     }
 
-    protected String cancelIndexRegex() {
-        return null;
+    /**
+     * 调用解析过滤器中判断只有被自定义解析的url地址内容才添加到solr索引，其余的忽略
+     * @param url
+     * @return
+     */
+    private boolean isMatchedForParseIndex(String url) {
+        boolean ok = false;
+        AbstractHtmlParseFilter[] parseFilters = AbstractHtmlParseFilter.getParseFilters(conf);
+        if (parseFilters != null) {
+            for (AbstractHtmlParseFilter htmlParseFilter : parseFilters) {
+                Boolean ret = htmlParseFilter.isUrlMatchedForParse(url);
+                //任何一个判断返回true表示为需要解析并进行索引的数据
+                if (ret == true) {
+                    ok = true;
+                    break;
+                }
+            }
+        }
+        return ok;
+    }
+
+    private static final Collection<WebPage.Field> FIELDS = new HashSet<WebPage.Field>();
+    static {
+        FIELDS.add(WebPage.Field.METADATA);
     }
 
     /**
@@ -54,7 +86,15 @@ public abstract class AbstractIndexingFilter implements IndexingFilter {
      */
     @Override
     public Collection<WebPage.Field> getFields() {
-        return null;
+        return FIELDS;
+    }
+
+    protected String getValueFromPageMetaData(WebPage page, String key) {
+        ByteBuffer bytes = page.getMetadata().get(new Utf8(key));
+        if (bytes == null) {
+            return null;
+        }
+        return Bytes.toString(bytes);
     }
 
     public abstract NutchDocument filterInternal(NutchDocument doc, String url, WebPage page);
--- apache-nutch-2.3/src/plugin/index-s2jh/src/java/org/apache/nutch/indexer/s2jh/S2jhIndexingFilter.java
+++ apache-nutch-2.3/src/plugin/index-s2jh/src/java/org/apache/nutch/indexer/s2jh/S2jhIndexingFilter.java
@@ -1,13 +1,17 @@
 package org.apache.nutch.indexer.s2jh;
 
+import java.util.Set;
+
 import org.apache.nutch.indexer.NutchDocument;
 import org.apache.nutch.storage.WebPage;
+import org.apache.nutch.util.TableUtil;
+import org.apache.solr.pinyin.ChineseToPinyinConvertor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * 
- * @author EMAIL:xautlx@hotmail.com , QQ:2414521719
+ * @author EMAIL:s2jh-dev@hotmail.com , QQ:2414521719
  *
  */
 public class S2jhIndexingFilter extends AbstractIndexingFilter {
@@ -16,8 +20,13 @@ public class S2jhIndexingFilter extends AbstractIndexingFilter {
 
     @Override
     public NutchDocument filterInternal(NutchDocument doc, String url, WebPage page) {
-        doc.add("biz", "123");
-
+        Set<String> pinyins = ChineseToPinyinConvertor.toPinyin(TableUtil.toString(page.getTitle()),
+                ChineseToPinyinConvertor.MODE.capital);
+        for (String pinyin : pinyins) {
+            doc.add("pinyin", pinyin);
+        }
+        doc.add("productPrice", getValueFromPageMetaData(page, "price"));
+        doc.add("productTitle", getValueFromPageMetaData(page, "title"));
         return doc;
     }
 }
